2713a5d2939fde69cc7b62fd27ddf9d0782d6e53
Erika Szabo <erika.szabo.geo@gmail.com>
2019-12-11 23:29:18 -0500

    uncommented importing App and index.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import counter from './reducer';
-// import App from './App';
-// import './index.css';
+import App from './App';
+import './index.css';
 
 var destination = document.querySelector('#container');
 
